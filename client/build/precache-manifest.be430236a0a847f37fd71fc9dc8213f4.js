cb3f48137f5d827bec310f93c0094b973a06ccb6
mhers91 <mhers91@gmail.com>
2019-11-13 12:53:15 -0500

    a commit

---
+++ client/build/precache-manifest.be430236a0a847f37fd71fc9dc8213f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760e1a7843db6a821acd5a18fd719e12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1040c3b9b7492355745",
+    "url": "/static/css/main.74e417be.chunk.css"
+  },
+  {
+    "revision": "195bd3eb227840b097f5",
+    "url": "/static/js/2.90858b5f.chunk.js"
+  },
+  {
+    "revision": "b1040c3b9b7492355745",
+    "url": "/static/js/main.c38971ea.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
